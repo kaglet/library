ce86b215e7975648136709ba55fd87a03977fd35
Kago <kmotlhabane769@gmail.com>
2023-12-18 22:25:59 +0200

    Update display selectively from add/remove
    Do not update and recreate the whole display from an add or remove,
    rather you can update all indices with a remove since that can happen
    at any spot. We add the logic to support this. The aim is to modify the
    list and the html not just the list alone while updating the entire
    UI layout cards instead of just removing and inserting specific ones
    but leaving the rest of the html unchanged where it does not need to be
    changed. Most of the computation is simply in the background
    now on a delete updating indices not on EVERY single add and delete.
    
    This might be slightly vague so revisit the previous commit to compare
    the outdated initial approach with its flaws with this new approach.

--- logic/script.js
+++ logic/script.js
@@ -3,18 +3,54 @@ const myLibrary = [];
 
 function Book() {}
 
-function createBook(title, author, pageNum, isRead) {
+function createBook(title, author, pageNum, isRead, id) {
     let newBook = Object.create(Book.prototype);
-    return newBook.init(title, author, pageNum, isRead);
+    return newBook.init(title, author, pageNum, isRead, id);
 }
 
 function addBookToLibrary() {
-    let newBook = createBook('title', 'author', 'pageNum', true);
+    let newBook = createBook('title', 'author', 'pageNum', true, myLibrary.length);
     myLibrary.push(newBook);
+
+    let card = document.createElement('section');
+    let bookContainer = document.querySelector('.books-display');
+    card.setAttribute('data-id', newBook.id);
+    card.textContent = 'I am book ' + newBook.id + ' ';
+    card.textContent += book.info();
+
+    let deleteBtn = document.createElement('button');
+    deleteBtn.setAttribute('type', 'button');
+    deleteBtn.textContent = 'Delete';
+    deleteBtn.addEventListener('click', () => {
+        removeBookFromLibrary(newBook.id);
+    });
+
+    bookContainer.appendChild(card);
+    // DO NOT NEED TO UPDATE BOOK INDICES
 }
 
 function removeBookFromLibrary(id) {
     myLibrary.splice(id, 1);
+    let bookContainer = document.querySelector('.books-display');
+    let bookToRemove = bookContainer.querySelector(`[data-id]="${id}"`);
+
+    bookContainer.remove(bookToRemove);
+    updateBookIndices();
+}
+
+function clearBookDisplay(bookContainer) {
+    let cardsList = document.querySelectorAll('.books-display > section');
+    if (cardsList.length === 0) {
+        return;
+    }
+
+    while (bookContainer.firstChild) {
+        bookContainer.removeChild(grid.lastChild);
+    }
+}
+
+function updateBookIndices() {
+    
 }
 
 function displayBooks() {
@@ -22,8 +58,15 @@ function displayBooks() {
         let card = document.createElement('section');
         let bookContainer = document.querySelector('.books-display');
         card.setAttribute('data-id', i);
-        card.textContent = 'I am book ' + i;
+        card.textContent = 'I am book ' + i + ' ';
         card.textContent += book.info();
+
+        let deleteBtn = document.createElement('button');
+        deleteBtn.setAttribute('type', 'button');
+        deleteBtn.textContent = 'Delete';
+        deleteBtn.addEventListener('click', () => {
+            removeBookFromLibrary(i);
+        });
 
         bookContainer.appendChild(card);
         // create new element to hold book card
@@ -32,11 +75,12 @@ function displayBooks() {
     });
 }
 
-Book.prototype.init = function (title, author, pageNum, isRead) {
+Book.prototype.init = function (title, author, pageNum, isRead, id) {
     this.title = title;
     this.author = author;
     this.pageNum = pageNum;
     this.isRead = isRead;
+    this.id = id;
     return this;
 };
 
@@ -55,25 +99,9 @@ Book.prototype.info = function () {
     return info;
 };
 
-addBookToLibrary();
-addBookToLibrary();
-addBookToLibrary();
-addBookToLibrary();
-addBookToLibrary();
-addBookToLibrary();
-addBookToLibrary();
-addBookToLibrary();
-addBookToLibrary();
-addBookToLibrary();
-addBookToLibrary();
-addBookToLibrary();
-addBookToLibrary();
-addBookToLibrary();
-addBookToLibrary();
-addBookToLibrary();
-addBookToLibrary();
-addBookToLibrary();
-addBookToLibrary();
-addBookToLibrary();
+for (let i = 0; i < 21; i++) {
+    addBookToLibrary();
+}
+
 displayBooks();
 console.dir(myLibrary);
