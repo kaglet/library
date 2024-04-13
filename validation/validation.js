a3592ea221d2e94ab8d57f029703e0048d62117d
Kago <kmotlhabane769@gmail.com>
2024-04-13 10:18:50 +0200

    Add error messages into form layout with error styles

---
+++ validation/validation.js
@@ -0,0 +1,21 @@
+const validateTextInput = (input) => {
+
+};
+
+const validateNumberInput = (input) => {
+
+};
+
+const validateURLInput = (input) => {
+
+};
+
+const displayErrorMessage = (errorDisplay, errorMessage) => {
+
+};
+
+const validateAll = () => {
+
+};
+
+export { validateAll, displayErrorMessage };
